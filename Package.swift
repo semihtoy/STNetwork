ec8e44ac4a8d4566496304e1577b72223ad6ad99
SemihTOY <semih.toy@halkbank.com.tr>
2023-10-06 00:39:05 +0300

    create spm

---
+++ Package.swift
@@ -0,0 +1,35 @@
+//
+//  Package.swift
+//  
+//
+//  Created by SemihTOY on 6.10.2023.
+//
+
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "STNetwork",
+    platforms: [.iOS(.v13)],  // Desteklenen platform ve sürüm
+    products: [
+        .library(
+            name: "STNetwork",
+            targets: ["STNetwork"]
+        ),
+    ],
+    dependencies: [
+        // Bağımlılıklarınızı burada belirtin. Örnek:
+         .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.0.0")
+    ],
+    targets: [
+        .target(
+            name: "STNetwork",
+            dependencies: []  // Eğer bağımlılıklarınız varsa bu kısıma ekleyin.
+        ),
+        .testTarget(
+            name: "STNetworkTests",
+            dependencies: ["STNetwork"]
+        ),
+    ]
+)
+
